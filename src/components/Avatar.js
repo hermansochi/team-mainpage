defb1bfb6ee25bd937bede8c03e870b9c13a6007
Herman Alexandrov <hermansochi@ya.ru>
2022-12-14 19:58:00 +0300

    Herman.Front: Add adptive design. Stage I.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -6,7 +6,7 @@ const Avatar = (props) => {
 return (
 		<li className={styles.avatar__item}>
 				<a href={ props.html_url } className={styles.avatar__link}>
-						<img src={ props.avatar_url } alt={ props.login } className={styles.avatar__img} width='100' height='100' />
+						<img src={ props.avatar_url } alt={ props.login } className={styles.avatar__img} />
 				</a>
 		</li>
 	)
